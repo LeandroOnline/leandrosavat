2364341e41e7af344d047cf4ea34a6c5ea8d3aef
LeandroOnline <105742869+LeandroOnline@users.noreply.github.com>
2023-03-04 13:37:48 -0300

    feat: finish Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { context } from "../App";
 import "./Merge.css";
 import "./Home.css";
-import perfil from "../assets/Perfil.jpg";
+import perfil from "../assets/Perfil2.png";
 
 function Home() {
   const { state } = useContext(context);
