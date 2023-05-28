1341d9ecd0b69d54f3e79ec1ccd2e1462106c8d5
LeandroOnline <leandrosavat@gmail.com>
2023-05-28 19:29:12 -0300

    feat: +1000 hours

--- src/utils/aboutTexts.js
+++ src/utils/aboutTexts.js
@@ -1,6 +1,6 @@
 const moreEnglish = [
   "I am motivated by the ability to provide solutions and help humanity through my software, it is an opportunity to solve frequent problems and optimize business processes. As a web developer, I have the ability to make a significant difference by contributing my skills and knowledge online.",
-  "+800 hours of programming in practices and studies. Organization and development of technology with Scrum methodology. Performance with integrity and work commitment. With the support of a big professional community. Problem solving and self-taught learning.",
+  "+1000 hours of programming in practices and studies. Organization and development of technology with Scrum methodology. Performance with integrity and work commitment. With the support of a big professional community. Problem solving and self-taught learning.",
   "Since I was 11 years old I have been working in my family business of audiovisual productions, where I developed a solid professional ethic and work commitment.",
   " I traveled for two years, making productions and acquiring English skills. During the trip, I met programmers who sparked my interest in coordinated work and web connection to the world.",
   "In 2021, I became interested in financial data programming which led me to learn about and focus on web programming.",
