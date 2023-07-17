65d77e907282340491e9b44a6a2a404ecce58fe8
LeandroOnline <leandrosavat@gmail.com>
2023-07-17 10:06:06 -0300

    update: CV

--- src/utils/aboutTexts.js
+++ src/utils/aboutTexts.js
@@ -1,5 +1,5 @@
 export const moreEnglish = [
-  "I am an expert in Web Engineering with a background in Design and Architecture. My greatest passion in the field of technology is the ability to face challenges and create simple and innovative solutions to meet important needs.",
+  "I am an expert in Web Engineering with a background in comprehensive Web Development, Design UI-UX and Web Architecture. My greatest passion in the field of technology is the ability to face challenges and create simple and innovative solutions to meet important needs.",
   "I like to constantly learn and push my limits, both professionally and personally. My path in programming began thanks to my constant desire to improve and my interest in discovering tools that would allow me to progress in both areas.",
   "Throughout my professional career, I have stood out for my leadership skills, my ability to work as a team, coordinate projects and optimize processes. Since I was 18 years old, I have been involved in the world of digital productions, leading an audiovisual production company where I provided photography, design and digital marketing services with development in different countries. This experience has allowed me to develop and refine my skills in these areas, bringing a comprehensive and multidisciplinary approach to my projects.",
   " I traveled for two years, making productions and acquiring English skills. During the trip, I met programmers who sparked my interest in coordinated work and web connection to the world.",
@@ -8,7 +8,7 @@ export const moreEnglish = [
 ];
 
 export const moreSpanish = [
-  "Soy experto en Ingeniería Web con formación en Diseño y Arquitectura. Mi mayor pasión en el campo de la tecnología es la capacidad de enfrentar desafíos y crear soluciones simples e innovadoras para satisfacer necesidades importantes.",
+  "Soy experto en Ingeniería Web con formación en Desarrollo Web integral, Diseño UI-UX y Arquitectura Web. Mi mayor pasión en el campo de la tecnología es la capacidad de enfrentar desafíos y crear soluciones simples e innovadoras para satisfacer necesidades importantes.",
   "Me gusta aprender y superar constantemente mis limites, tanto a nivel profesional como personal. Mi camino en la programación comenzó gracias a mi constante deseo de superación y mi interés por descubrir herramientas que me permitieran progresar en ambos ámbitos.",
   "A lo largo de mi trayectoria profesional, he destacado por mi habilidad de liderazgo, mi capacidad para trabajar en equipo, coordinar proyectos y optimizar procesos. Desde los 18 años, he estado involucrado en el mundo de las producciones digitales, liderando una productora audiovisual donde brindaba servicios de fotografía, diseño y marketing digital con desarrollo en diferentes paises. Esta experiencia me ha permitido desarrollar y perfeccionar mis habilidades en estas áreas, aportando un enfoque integral y multidisciplinario a mis proyectos.",
   "Viajé durante dos años, haciendo producciones y adquiriendo conocimientos de inglés. Durante el viaje conocí a programadores que despertaron mi interés por el trabajo coordinado y la conexión web con el mundo.",
