32b239d412c4cd936eb10f648699bdac3ea0f14f
LeandroOnline <leandrosavat@gmail.com>
2023-07-19 19:58:48 -0300

    feat: take out poor experiences

--- src/utils/experience.js
+++ src/utils/experience.js
@@ -6,25 +6,10 @@ export const experienceEnglish = [
     details: "Web Application Programming",
   },
   {
-    title: "VOLUNTEER",
-    since: "Since May 2022",
-    subtitle: "Academic group",
-    details:
-      "BootCamps (Henry / Open Bootcamp / Linkedin) + Curriculum Consultant",
-  },
-  {
     title: "DEVELOPER",
     since: "2021-2022",
     subtitle: "Data graph visualization FRONTEND in Pine Script",
     details: "Graphics programming for financial data visualization",
-  },
-  ,
-  {
-    title: "ENGLISH",
-    since: "2019-2021",
-    subtitle: "Improvement of the level of English during a global trip",
-    details:
-      "Development of multicultural communication skills and adaptability",
   },
   {
     title: "CO-FOUNDER",
@@ -37,14 +22,7 @@ export const experienceEnglish = [
     since: "2008-2010",
     subtitle: "Software TECHNICIAN in Ciber",
     details: "Software and hardware administration of 30 computers",
-  },
-  {
-    title: "STUDENT",
-    since: "2007-2010",
-    subtitle: "And EMPLOYEE At UTN University",
-    details:
-      "Engineering student in Information Systems + Employee in Photocopier",
-  },
+  }
 ];
 
 export const experienceSpanish = [
@@ -55,25 +33,10 @@ export const experienceSpanish = [
     details: "Programacion de Aplicaciones Web",
   },
   {
-    title: "VOLUNTARIO",
-    since: "Desde Mayo 2022",
-    subtitle: "Grupo Academico",
-    details:
-      "BootCamps (Henry / Open Bootcamp / Linkedin) + Curriculum Consultant",
-  },
-  {
     title: "DESARROLLADOR",
     since: "2021-2022",
     subtitle: "Desarrollo FrontEnd de graficos para visualizacion de datos en Pine Script",
     details: "Graphics programming for financial data visualization",
-  },
-  ,
-  {
-    title: "INGLES",
-    since: "2019-2021",
-    subtitle: "Mejora del nivel de inglés durante un viaje global",
-    details:
-      "Desarrollo de habilidades de comunicación multicultural y adaptabilidad.",
   },
   {
     title: "CO-FOUNDADOR",
@@ -86,12 +49,5 @@ export const experienceSpanish = [
     since: "2008-2010",
     subtitle: "Tecnico de Software en Ciber",
     details: "Administracion de software y hardware de computadoras",
-  },
-  {
-    title: "ESTUDIANTE",
-    since: "2007-2010",
-    subtitle: "UTN Universidad",
-    details:
-      "Estudiante de ingenieria en sistemas de informacion",
-  },
+  }
 ];
