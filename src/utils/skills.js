def8716807d117dcc0b7384b1db0a9bb05715d70
LeandroOnline <leandrosavat@gmail.com>
2023-07-31 11:47:22 -0300

    feat: orders skills

--- src/utils/skills.js
+++ src/utils/skills.js
@@ -6,7 +6,6 @@ import html from "../assets/icons/html.png";
 import css from "../assets/icons/css.png";
 import js from "../assets/icons/js.png";
 import react from "../assets/icons/react.png";
-import docker from "../assets/icons/docker.png";
 import node from "../assets/icons/nodejs.png";
 import express from "../assets/icons/express.png";
 import mongodb from "../assets/icons/mongodb.png";
@@ -15,19 +14,18 @@ import notion from "../assets/icons/notion.png";
 
 const skills = [
   { scrum },
-  { wordpress },
   { photoshop },
-  { git },
+  { notion },
+  { figma },
   { html },
   { css },
   { js },
   { react },
-  { docker },
+  { git },
+  { wordpress },
   { node },
   { express },
   { mongodb },
-  { figma },
-  { notion },
 ];
 
 export default skills;
