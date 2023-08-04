ef6b63e73fdf245d65331e05ffed3df13421c208
LeandroOnline <leandrosavat@gmail.com>
2023-08-04 19:27:40 -0300

    feat: format

--- src/utils/driven.js
+++ src/utils/driven.js
@@ -4,6 +4,7 @@ import meditacion from "../assets/interest/meditacion.png";
 import paisaje from "../assets/interest/paisaje.png";
 import personas from "../assets/interest/personas.png";
 import escribir from "../assets/interest/escribir.png";
+
 const driven = [dieta, kayak, meditacion, paisaje, , personas, escribir];
 
 export default driven;
